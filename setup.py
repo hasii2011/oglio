6e35b3f1f01da52d6a3ebbabb1938612c365acb5
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-11-02 14:49:43 -0700

    <type>[optional scope]: <description>
    [optional body]
    
    [optional footer(s)]

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ LICENSE = (HERE / 'LICENSE').read_text()
 
 setup(
     name="oglio",
-    version="0.5.7",
+    version="0.5.8",
     author_email='Humberto.A.Sanchez.II@gmail.com',
     maintainer='Humberto A. Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
     description='External Pyut Persistence',
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/hasii2011/xmlio",
+    url="https://github.com/hasii2011/oglio",
     packages=[
         'oglio', 'oglio.toXmlV10'
     ],
@@ -26,8 +26,9 @@ setup(
         'oglio.toXmlV10': ['py.typed']
     },
     install_requires=[
+        'wxPython==4.2.0'
         'pyutmodel==1.1.0',
-        'ogl==0.60.5',
-        'untanglepyut==0.5.1',
+        'ogl==0.60.6',
+        'untanglepyut==0.5.3',
     ],
 )
