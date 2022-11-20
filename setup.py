c4ffb0a082a7fbfa33a89a10c383bb16172e5d29
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-11-19 18:17:33 -0700

    <>[]: <Bump Ogl>
    []
    
    []

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ LICENSE = (HERE / 'LICENSE').read_text()
 
 setup(
     name="oglio",
-    version="0.5.15",
+    version="0.5.16",
     author_email='Humberto.A.Sanchez.II@gmail.com',
     maintainer='Humberto A. Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
@@ -27,8 +27,8 @@ setup(
     },
     install_requires=[
         'wxPython==4.2.0',
-        'pyutmodel==1.2.0',
-        'ogl==0.60.9',
-        'untanglepyut==0.6.0',
+        'pyutmodel~=1.2.0',
+        'ogl~=0.60.10',
+        'untanglepyut~=0.6.1',
     ],
 )
