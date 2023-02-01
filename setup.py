3e2df65f3df7443b8b10f617e04b4c807e8961a2
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-01-31 18:37:29 -0700

    <>[]: <Bump dependencies>
    [
    * bump Ogl
    * bump untanglepyut
    ]
    
    [optional footer(s)]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ LICENSE = (HERE / 'LICENSE').read_text()
 
 setup(
     name="oglio",
-    version="0.5.50",
+    version="0.5.51",
     author_email='Humberto.A.Sanchez.II@gmail.com',
     maintainer='Humberto A. Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
