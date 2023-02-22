bbf6c2fb5c607606c1d58c2486d77fbd6b599861
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-02-21 19:18:00 -0700

    <>[]: <Dependency Update>
    []
    
    []

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'wxPython==4.2.0',
         'pyutmodel~=1.4.0',
-        'ogl~=0.70.0',
-        'untanglepyut~=0.6.25',
+        'ogl~=0.70.10',
+        'untanglepyut~=0.6.30',
     ],
 )
